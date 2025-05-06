2e8823ff028de2daef06322b34140e029e7e2f5d
nickleezx <nickleezx@gmail.com>
2025-05-07 03:16:05 +0800

    ex 3.15 - 3.18 completed

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Repository for deployment
 
-The purpose of this repository is for deployment ex 3.9 - 3.11 of part 3 of FullStackOpen Web Development course 
+The purpose of this repository is for deployment ex 3.9 - 3.14 of part 3 of FullStackOpen Web Development course 
 
 Reference: https://fullstackopen.com/en/part3/deploying_app_to_internet
--- index.js
+++ index.js
@@ -1,95 +1,95 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const Phonebook = require('./models/phoneBook');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const Phonebook = require("./models/phoneBook");
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 app.use(express.json());
 
 const logger = morgan(function (tokens, req, res) {
     return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      tokens['posted-msg'] = JSON.stringify(req.body),
-    ].join(' ')
-  })
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, "content-length"),
+        "-",
+        tokens["response-time"](req, res),
+        "ms",
+        (tokens["posted-msg"] = JSON.stringify(req.body)),
+    ].join(" ");
+});
 
 app.use(logger);
 
-
 let phoneBook = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        id: "1",
+        name: "Arto Hellas",
+        number: "040-123456",
     },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        id: "2",
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
     },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        id: "3",
+        name: "Dan Abramov",
+        number: "12-43-234345",
     },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+    {
+        id: "4",
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+    },
+];
 
+app.get("/api/persons", (request, response) => {
+    Phonebook.find({})
+        .then((result) => {
+            response.json(result);
+        })
+        .catch((e) => {
+            console.log("Error fetching all persons from database", e.message);
+        });
+});
 
-app.get('/api/persons', (request, response) => {
-    Phonebook.find({}).then(result => {
-        response.json(result);
-    });
-})
-
-app.get('/info', (request, response) => {
-    Phonebook.find({}).then(result => {
-        const length = result.length
+app.get("/info", (request, response, next) => {
+    Phonebook.find({}).then((result) => {
+        const length = result.length;
         response.send(`<p>Phonebook has info for ${length} people</p>
-                        <p>${new Date()}</p>`)
-    })
-    
-})
+                        <p>${new Date()}</p>`);
+    });
+});
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
+app.get("/api/persons/:id", (request, response) => {
+    Phonebook.findById(request.params.id)
+        .then(result => {
+            response.json(result)
+        })
+        .catch(error => next(error));
+});
 
-    const person = phoneBook.find(p => p.id === id);
-
-    if (!person){
-        return response.status(404).send('Person not found');
-    }
-
-    response.json(person);
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-
-    phoneBook = phoneBook.filter(p => p.id !== id);
-    response.status(204).end();
-})
+app.delete("/api/persons/:id", (request, response, next) => {
+    Phonebook.findByIdAndDelete(request.params.id)
+        .then((contact) => {
+            response.status(204).end();
+        })
+        .catch((error) => next(error));
+});
 
 const generateId = () => {
     return String(Math.floor(Math.random() * 1001));
-}
+};
 
-app.post('/api/persons', (request, response) => {
+app.post("/api/persons", (request, response) => {
     const body = request.body;
 
     if (!body.name || !body.number)
         return response.status(400).json({
-            error: "No name or phone number indicated"
-        })
+            error: "No name or phone number indicated",
+        });
 
     // const found = phoneBook.find(p => p.name.toLowerCase() === body.name.toLowerCase())
 
@@ -97,20 +97,48 @@ app.post('/api/persons', (request, response) => {
     //     return response.status(400).json({
     //         error: `name must be unique`,
     //     })
-    
+
     const person = new Phonebook({
         name: body.name,
         number: body.number,
-    })
-
-    person.save().then(result => {
-        response.json(result);
     });
-})
 
+    person
+        .save()
+        .then((result) => {
+            response.json(result);
+        })
+        .catch((e) =>
+            console.log("Error saving person to database", e.message)
+        );
+});
 
+app.put("/api/persons/:id", (request, response, next) => {
+    const person = {
+        name: request.body.name,
+        number: request.body.number,
+    };
+
+    Phonebook.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then((updatedPerson) => {
+            response.json(updatedPerson);
+        })
+        .catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message);
+
+    next(error);
+};
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
-})
+});
